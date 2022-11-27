048e9910e15e90e24e9031c74c783f70cf63806d
Лера <Lera290897@gmail.com>
2022-11-27 16:52:17 +0300

    Статистика

---
+++ src/main/java/StartsServise.java
@@ -0,0 +1,56 @@
+public class StartsServise {
+
+
+    public long sum(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long medium(long[] sales) {
+        return sum(sales) / 12;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return maxMonth + 1;
+    }
+
+    public int higherMedium(long[] sales) {
+        int count = 0;
+        long medium = medium(sales);
+        for (long sale : sales) {
+            if (sale > medium) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
+
